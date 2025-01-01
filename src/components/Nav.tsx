e384a7920a119178f17000e58a54dfcb570f7b6a
Caryn Carter <carter.caryn@gmail.com>
2025-01-01 17:29:55 -0600

    modified navigation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,55 @@
+import { Link, useLocation } from 'react-router-dom';
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
+  const currentPage = useLocation().pathname;
+
+  // TODO: Add necessary code to display the navigation bar and link between the pages (none of this was here, not the import statement or anything after return)
   return (
-    <div>Nav</div>
-  )
+    <nav>
+    <h1>
+      <Link to='/' id='logo'>
+        Candidate Search
+      </Link>
+    </h1>
+    <ul className='nav nav-tabs'>
+      <li className='nav-item'>
+        <h2>
+          <Link
+            to='/'
+            className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+          >
+            HOME
+          </Link>
+        </h2>
+      </li>
+      <li className='nav-item'>
+        <h2>
+          <Link
+            to='/search'
+            className={
+              currentPage === '/search' ? 'nav-link active' : 'nav-link'
+            }
+          >
+            SEARCH FOR CANDIDATES
+          </Link>
+        </h2>
+      </li>
+      <li className='nav-item'>
+        <h2>
+          <Link
+            to='/saved'
+            className={
+              currentPage === '/saved' ? 'nav-link active' : 'nav-link'
+            }
+          >
+            SAVED CANDIDATES
+          </Link>
+        </h2>
+      </li>
+    </ul>
+  </nav>
+  );
+
 };
 
 export default Nav;
