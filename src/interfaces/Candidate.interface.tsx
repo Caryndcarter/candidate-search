666bf40a9153ee73bb6e204b2acdb2f3090859fa
Caryn Carter <carter.caryn@gmail.com>
2025-01-01 18:24:57 -0600

    added Candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,13 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+// TODO: Create an interface for the Candidate objects returned by the API -- the avatar_url is the image, and the login is the username
+
+
+export default interface Candidate {
+    readonly Name: string | null;
+    readonly Email: string | null;
+    readonly Company: string | null;
+    readonly Bio: string | null;
+    readonly Login: string | null;
+    readonly Image: string | null;
+    readonly Location: string | null;
+  }
+  
