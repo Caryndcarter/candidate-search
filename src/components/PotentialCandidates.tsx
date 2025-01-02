b7d2675e32bba41d33ab78caa87b171e0da8274a
Caryn Carter <carter.caryn@gmail.com>
2025-01-01 19:18:36 -0600

    added potential candidates list component

---
+++ src/components/PotentialCandidates.tsx
@@ -0,0 +1,38 @@
+import type React from 'react';
+import type Candidate from '../interfaces/Candidate.interface';
+import CandidateCard from './CandidateCard';
+
+interface PotentialCandidatesProps {
+  potentialCandidates: Candidate[];
+  removeFromStorage:
+    | ((
+        e: React.MouseEvent<SVGSVGElement, MouseEvent>,
+        currentlyOnPotnentialList: boolean | null | undefined,
+        login: string | null
+      ) => void)
+    | null;
+}
+
+const PotentialCandidatesList = ({
+  potentialCandidates,
+  removeFromStorage,
+}: PotentialCandidatesProps) => {
+  console.log(potentialCandidates);
+
+  return (
+    <>
+      <ul>
+        {potentialCandidates.map((candidate) => (
+          <CandidateCard
+            currentCandidate={candidate}
+            key={candidate.Login}
+            onPotentialList={true}
+            removeFromStorage={removeFromStorage}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default PotentialCandidatesList;
