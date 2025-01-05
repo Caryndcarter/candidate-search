66495810e5b7e5eb94a5b6a76f8680f42d7f680d
Caryn Carter <carter.caryn@gmail.com>
2025-01-04 20:00:44 -0600

    added SavedCandidates page with mapped list of candidate rows

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-
-/*import { useState, useEffect, useCallback } from 'react';
-import { searchGithub, searchGithubUser } from '../api/API';
-//import CandidateCard from '../components/CandidateCard';
+import { useState, useEffect } from 'react';
+import PotentialCandidate from '../components/PotentialCandidate';
 import type Candidate from '../interfaces/Candidate.interface';
 
+
 const removeFromStorage= async (
-  currentlyOnPotentialList: boolean | null | undefined,
-  login: string | null
+  login: string | null, 
+  
 ) => {
-  if (currentlyOnPotentialList && login) {
+  if (login) {
     const storedPotentialCandidates = localStorage.getItem('potentialCandidates');
     if (storedPotentialCandidates) {
       const parsedPotentialCandidates: Candidate[] = JSON.parse(storedPotentialCandidates);
@@ -23,24 +22,32 @@ const removeFromStorage= async (
   }
 };
 
-
-const onPotentialList = useCallback(() => {
-  const storedPotentialCandidates = localStorage.getItem('potentialCandidates');
-  if (storedPotentialCandidates) {
-    const parsedPotentialCandidates: Candidate[] = JSON.parse(storedPotentialCandidates);
-    // Check if the current candidate's login is already in the list
-    return parsedPotentialCandidates.some(candidate => candidate.Login === currentCandidate.Login);
-  }
-  return false;
-}, [currentCandidate]);
-*/
-
 const SavedCandidates = () => {
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
+
+  useEffect(() => {
+    const storedPotentialCandidates = localStorage.getItem('potentialCandidates');
+    if (storedPotentialCandidates) {
+      const parsedPotentialCandidates: Candidate[] = JSON.parse(storedPotentialCandidates);
+      setCandidates(parsedPotentialCandidates);
+    }
+  }, []);
+
   return (
     <>
       <h1>Potential Candidates</h1>
+      <div className="candidate-list">
+        {candidates.map((candidate) => (
+          <PotentialCandidate
+            key={candidate.Login}
+            potentialCandidate={candidate}
+            removeFromStorage={removeFromStorage}
+          />
+        ))}
+      </div>
     </>
   );
 };
+
 
 export default SavedCandidates;
